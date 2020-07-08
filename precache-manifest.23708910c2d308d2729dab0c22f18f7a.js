83833ae11f0775eddf7f861079256e8ecae8db3e
worlaalex <mightyworla@gmail.com>
2020-07-08 02:30:02 +0000

    Updates

---
+++ precache-manifest.23708910c2d308d2729dab0c22f18f7a.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e8e4ad5f2e66941aff29427942e5ace",
+    "url": "/profile/index.html"
+  },
+  {
+    "revision": "02730ef281d26d718ecb",
+    "url": "/profile/static/css/3.cffc9e54.chunk.css"
+  },
+  {
+    "revision": "9ec6709fa311f61f8747",
+    "url": "/profile/static/css/4.54df59d4.chunk.css"
+  },
+  {
+    "revision": "1ca68420d72478cde361",
+    "url": "/profile/static/css/main.4cc58fcd.chunk.css"
+  },
+  {
+    "revision": "c508e64ed31397069fa6",
+    "url": "/profile/static/js/2.3431f8f6.chunk.js"
+  },
+  {
+    "revision": "02730ef281d26d718ecb",
+    "url": "/profile/static/js/3.20be5a2f.chunk.js"
+  },
+  {
+    "revision": "9ec6709fa311f61f8747",
+    "url": "/profile/static/js/4.bd85cf93.chunk.js"
+  },
+  {
+    "revision": "1ca68420d72478cde361",
+    "url": "/profile/static/js/main.5060cb7b.chunk.js"
+  },
+  {
+    "revision": "bbac63c07cca0d83ff4e",
+    "url": "/profile/static/js/runtime-main.ec6ecc2f.js"
+  },
+  {
+    "revision": "af3676b7295bc63aa7084c6258c04567",
+    "url": "/profile/static/media/ACDI-logo.af3676b7.png"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/profile/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "e8677c7a5fda2cb1fac023b7fd31e704",
+    "url": "/profile/static/media/Cartoon-illustration-education-vector.e8677c7a.jpg"
+  },
+  {
+    "revision": "49be0a71f35f04dd410a31d7a58e1bfd",
+    "url": "/profile/static/media/GraphQL.49be0a71.png"
+  },
+  {
+    "revision": "242d0142fcccd6b3ac1c1485e40e82d7",
+    "url": "/profile/static/media/LexerCodesGlobal01512-1.242d0142.png"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/profile/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "563b7f6b8f538950221e7a88f9583e78",
+    "url": "/profile/static/media/Notitia Ai512.563b7f6b.png"
+  },
+  {
+    "revision": "5eff4f39f0cb2d795f9b458ec6e19725",
+    "url": "/profile/static/media/Our Roots Foundation.5eff4f39.jpeg"
+  },
+  {
+    "revision": "68e841388612be0c5143e2932fb77d4f",
+    "url": "/profile/static/media/contactMail.68e84138.webp"
+  },
+  {
+    "revision": "aa7d2090af504efa92cd23b9e518b6dd",
+    "url": "/profile/static/media/engineer.aa7d2090.jpeg"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/profile/static/media/logo.25bf045c.svg"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/profile/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "0288c5ec7a87ea73f8d14b12fc4bc94e",
+    "url": "/profile/static/media/talksCardBack.0288c5ec.svg"
+  },
+  {
+    "revision": "279da0eddd5cf914d29ec923e837e3fe",
+    "url": "/profile/static/media/working_man.279da0ed.gif"
+  }
+]);
